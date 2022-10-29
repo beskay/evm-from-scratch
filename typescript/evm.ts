5c30a5d704ef8f2e6907486b384cbba7466adef6
beskay <beskay0x@gmail.com>
2022-10-29 20:34:48 +0200

    more opcodes

--- typescript/evm.ts
+++ typescript/evm.ts
@@ -188,10 +188,41 @@ export default function evm(code: Uint8Array) {
         stack.unshift(MAX_UINT256 ^ a);
         break;
       }
+      // BYTE
+      case 0x1a: {
+        // pop first two stack items
+        let a = stack.shift() as bigint; // offset
+        let b = stack.shift() as bigint; // value
+
+        stack.unshift(a < 32 ? (b >> ((31n - a) * 8n)) & 0xffn : 0n);
+        break;
+      }
       // POP
       case 0x50: {
         // shift is like pop, but removes the first element in the array
         stack.shift();
+        break;
+      }
+      // JUMP
+      case 0x56: {
+        let a = stack.shift() as bigint; // JUMPDEST
+
+        // change program counter to jumpdest
+        i = Number(a);
+        break;
+      }
+      // JUMPI
+      case 0x57: {
+        // pop first two stack items
+        let a = stack.shift() as bigint; // JUMPDEST
+        let b = stack.shift() as bigint; // Condition (1 = jump, 0 = continue as usual)
+
+        if (b != 0n) i = Number(a);
+        break;
+      }
+      // PC
+      case 0x58: {
+        stack.unshift(BigInt(i));
         break;
       }
       // PUSH1
@@ -200,15 +231,302 @@ export default function evm(code: Uint8Array) {
         stack.unshift(BigInt(code[++i])); // have to transform to bigint to append an "n"
         break;
       }
-      // PUSH32
-      case 0x7f: {
+      case 0x61: {
         let tmp: string = "";
-        // convert the next 32 uint8 vars to hex string and concatenate them
-        for (let j = 0; j < 32; j++) tmp += code[++i].toString(16);
+        for (let j = 0; j < 2; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
         // convert from hex string to bigint and push to stack
         stack.unshift(BigInt(`0x${tmp}`));
         break;
       }
+      case 0x62: {
+        let tmp: string = "";
+        for (let j = 0; j < 3; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x63: {
+        let tmp: string = "";
+        for (let j = 0; j < 4; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x64: {
+        let tmp: string = "";
+        for (let j = 0; j < 5; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x65: {
+        let tmp: string = "";
+        for (let j = 0; j < 6; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x66: {
+        let tmp: string = "";
+        for (let j = 0; j < 7; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x67: {
+        let tmp: string = "";
+        for (let j = 0; j < 8; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x68: {
+        let tmp: string = "";
+        for (let j = 0; j < 9; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x69: {
+        let tmp: string = "";
+        for (let j = 0; j < 10; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x6a: {
+        let tmp: string = "";
+        for (let j = 0; j < 11; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x6b: {
+        let tmp: string = "";
+        for (let j = 0; j < 12; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x6c: {
+        let tmp: string = "";
+        for (let j = 0; j < 13; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x6d: {
+        let tmp: string = "";
+        for (let j = 0; j < 14; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x6e: {
+        let tmp: string = "";
+        for (let j = 0; j < 15; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x6f: {
+        let tmp: string = "";
+        for (let j = 0; j < 16; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x70: {
+        let tmp: string = "";
+        for (let j = 0; j < 17; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x71: {
+        let tmp: string = "";
+        for (let j = 0; j < 18; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x72: {
+        let tmp: string = "";
+        for (let j = 0; j < 19; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x73: {
+        let tmp: string = "";
+        for (let j = 0; j < 20; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x74: {
+        let tmp: string = "";
+        for (let j = 0; j < 21; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x75: {
+        let tmp: string = "";
+        for (let j = 0; j < 22; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x76: {
+        let tmp: string = "";
+        for (let j = 0; j < 23; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x77: {
+        let tmp: string = "";
+        for (let j = 0; j < 24; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x78: {
+        let tmp: string = "";
+        for (let j = 0; j < 25; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x79: {
+        let tmp: string = "";
+        for (let j = 0; j < 26; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x7a: {
+        let tmp: string = "";
+        for (let j = 0; j < 27; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x7b: {
+        let tmp: string = "";
+        for (let j = 0; j < 28; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x7c: {
+        let tmp: string = "";
+        for (let j = 0; j < 29; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x7d: {
+        let tmp: string = "";
+        for (let j = 0; j < 30; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x7e: {
+        let tmp: string = "";
+        for (let j = 0; j < 31; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      case 0x7f: {
+        let tmp: string = "";
+        // convert the next 32 uint8 vars to hex string and concatenate them
+        for (let j = 0; j < 32; j++)
+          tmp += code[++i].toString(16).padStart(2, "0");
+        // convert from hex string to bigint and push to stack
+        stack.unshift(BigInt(`0x${tmp}`));
+        break;
+      }
+      // DUP1
+      case 0x80: {
+        stack.unshift(stack[0]);
+        break;
+      }
+      // DUP2
+      case 0x81: {
+        stack.unshift(stack[1]);
+        break;
+      }
+      // DUP3
+      case 0x82: {
+        stack.unshift(stack[2]);
+        break;
+      }
+      // DUP4
+      case 0x83: {
+        stack.unshift(stack[3]);
+        break;
+      }
+      // DUP5
+      case 0x84: {
+        stack.unshift(stack[4]);
+        break;
+      }
+      // SWAP1
+      case 0x90: {
+        let tmp = stack[1];
+        stack[1] = stack[0];
+        stack[0] = tmp;
+        break;
+      }
+      // SWAP2
+      case 0x91: {
+        let tmp = stack[2];
+        stack[2] = stack[0];
+        stack[0] = tmp;
+        break;
+      }
+      // SWAP3
+      case 0x92: {
+        let tmp = stack[3];
+        stack[3] = stack[0];
+        stack[0] = tmp;
+        break;
+      }
+
       // default case for non implemented opcodes
       default: {
         break;
